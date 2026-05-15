a625b4831a2a2fb5c94ddd0391683a6735408d95
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-05-14 21:50:52 -0300

    UserEntity

--- src/main/java/com/dscommerce/dscommerce/entities/User.java
+++ src/main/java/com/dscommerce/dscommerce/entities/User.java
@@ -3,6 +3,9 @@ package com.dscommerce.dscommerce.entities;
 import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 
@@ -10,6 +13,8 @@ import jakarta.persistence.Table;
 @Table(name="tb_user")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
