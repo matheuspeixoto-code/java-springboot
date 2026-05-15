8b5f51703609f3cb48b34c3eea5b51a5e4fc4327
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-05-15 14:25:51 -0300

    refactor(Entity): refatorando entidade produto para ter a lista de OrderItems

--- src/main/java/com/dscommerce/dscommerce/entities/Product.java
+++ src/main/java/com/dscommerce/dscommerce/entities/Product.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -37,6 +38,9 @@ public class Product {
 			inverseJoinColumns = @JoinColumn(name="category_id")
 			)
 	private Set<Category> categories = new HashSet<>();
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Product() {
 		
