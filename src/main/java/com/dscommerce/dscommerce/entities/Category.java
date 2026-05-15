8a86ced3138ae9e838a6e6743ce31f837055369f
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-05-15 13:57:19 -0300

    refactor(Entity): Inserindo notacoes e relacionamento

--- src/main/java/com/dscommerce/dscommerce/entities/Category.java
+++ src/main/java/com/dscommerce/dscommerce/entities/Category.java
@@ -1,5 +1,59 @@
 package com.dscommerce.dscommerce.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_category")
 public class Category {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
+
+	public Category() {
+		
+	}
+	
+	public Category(Long id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+	
+	
+	
 }
