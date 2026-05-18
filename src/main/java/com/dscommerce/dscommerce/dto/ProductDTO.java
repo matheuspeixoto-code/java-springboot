bef722c97e216fef53a33929c20a43f64d9c248a
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-05-18 20:34:12 -0300

    feat(dto): inserindo os atributos no ProductDTO

--- src/main/java/com/dscommerce/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/dscommerce/dscommerce/dto/ProductDTO.java
@@ -1,5 +1,62 @@
 package com.dscommerce.dscommerce.dto;
 
+import com.dscommerce.dscommerce.entities.Product;
+
 public class ProductDTO {
+	private Long id;
+	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
+	
+	
+	public ProductDTO() {
+		
+	}
+
+
+	public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
+	}
+	
+	public ProductDTO(Product entity) {
+		super();
+		this.id = entity.getId();
+		this.name = entity.getName();
+		this.description = entity.getDescription();
+		this.price = entity.getPrice();
+		this.imgUrl = entity.getImgUrl();
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public Double getPrice() {
+		return price;
+	}
+
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+	
 	
 }
