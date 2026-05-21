5a49503fcb6b5b96b2ea9c3462653f2e95da4547
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-05-21 18:02:33 -0300

    feat(service): service para criacao de produtos

--- src/main/java/com/dscommerce/dscommerce/services/ProductService.java
+++ src/main/java/com/dscommerce/dscommerce/services/ProductService.java
@@ -38,4 +38,18 @@ public class ProductService {
 		
 		return result.map(x-> new ProductDTO(x));
 	}
+	
+	@Transactional
+	public ProductDTO insert(ProductDTO dto) {
+		Product entity = new Product();
+		
+		entity.setDescription(dto.getDescription());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setName(dto.getName());
+		entity.setPrice(dto.getPrice());
+		
+		entity = repository.save(entity);
+		
+		return new ProductDTO(entity);
+	}
 }
