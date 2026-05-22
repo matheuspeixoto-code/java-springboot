3cfcd60f486795048da41dd959c671999120b452
Matheus Peixoto Pereira <matheuspeixoto920@gmail.com>
2026-05-21 21:22:51 -0300

    feat(Exceptions): Inserindo o DataBaseException no ControllerAdvice

--- src/main/java/com/dscommerce/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/dscommerce/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.dscommerce.dscommerce.dto.CustomError;
+import com.dscommerce.dscommerce.services.exceptions.DataBaseException;
 import com.dscommerce.dscommerce.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,6 +19,13 @@ public class ControllerExceptionHandler {
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e,HttpServletRequest request) {
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<CustomError> DataBase(DataBaseException e,HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
